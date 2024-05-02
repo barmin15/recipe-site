377d2d1ad0b42b38257775c326ab82d0ff1ffb09
barmin15 <115711430+barmin15@users.noreply.github.com>
2024-05-02 14:27:44 +0200

    create: home page

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+//pages
+import HomePage from './pages/home/HomePage';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
      <BrowserRouter>
       <Routes>
-        <Route path="/" element={'home page'} />
+        <Route path="/" element={<HomePage/>} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
