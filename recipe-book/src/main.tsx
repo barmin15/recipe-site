b0ee42d582221e4697c156c5b370370cabfd108b
barmin15 <115711430+barmin15@users.noreply.github.com>
2024-05-02 14:17:03 +0200

    add: react router

--- recipe-book/src/main.tsx
+++ recipe-book/src/main.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={'home page'} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
