18bcd674cb60871c0d0c19f4580989a6f842bfd7
barmin15 <115711430+barmin15@users.noreply.github.com>
2024-05-03 11:32:57 +0200

    create: open recipe component

---
+++ src/pages/home/components/OpenRecipe.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { getRequest } from "../../../api/fetch";
+import { Recipe } from "../../../data/recipeDatas";
+import { Typography, List, ListItem, ListItemText } from "@mui/material";
+
+interface OpenRecipeProp {
+    id: number;
+}
+
+export default function OpenRecipe({ id }: OpenRecipeProp) {
+    const [recipe, setRecipe] = useState<Recipe>({
+        id: 0,
+        name: '',
+        description: '',
+        preparationSteps: '',
+        ingredients: []
+    });
+
+    useEffect(() => {
+        getRequest(`/recipes/${id}`)
+            .then(res => setRecipe(res))
+            .catch(err => console.log(err));
+    }, [id]);
+
+    return (
+        <>
+            <Typography variant="h5" gutterBottom>
+                {recipe.name}
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+                <strong>Leírás:</strong> {recipe.description}
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                <strong>Elkészítési lépések:</strong> {recipe.preparationSteps}
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+                <strong>Hozzávalók:</strong>
+                <List>
+                    {recipe.ingredients.map((ingredient, index) => (
+                        <ListItem key={index}>
+                            <ListItemText primary={`${ingredient.amount} ${ingredient.unitName} ${ingredient.ingredientName}`} />
+                        </ListItem>
+                    ))}
+                </List>
+            </Typography>
+        </>
+    );
+}
