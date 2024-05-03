0b5764395571a2024496d34ed4880efa695d8f78
barmin15 <115711430+barmin15@users.noreply.github.com>
2024-05-03 18:48:52 +0200

    add: mobile navigation

--- src/pages/editRecipe/components/NavigateBack.tsx
+++ src/pages/editRecipe/components/NavigateBack.tsx
@@ -1,21 +1,25 @@
 import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
 import { useNavigate } from 'react-router-dom';
+import { isMobile } from 'react-device-detect';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 export default function NavigateBack() {
     const navigate = useNavigate();
 
-    return (
-        <Box
+    return isMobile ?
+        <ArrowBackIosIcon
             sx={{
                 position: 'fixed',
                 top: 20,
                 left: 20,
+                fontSize: 30,
+                color: 'black',
+                cursor: 'pointer'
             }}
-        >
-            <Button variant="contained" onClick={() => navigate("/")}>
-                Vissza
-            </Button>
-        </Box>
-    );
+            onClick={() => navigate("/")}
+        />
+        :
+        <Button variant="contained" sx={{ position: 'fixed', top: 20, left: 20 }} onClick={() => navigate("/")}>
+            Vissza
+        </Button>
 };
