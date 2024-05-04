7f6432618fe8edf08f5cef7b4559848a3041e60b
barmin15 <115711430+barmin15@users.noreply.github.com>
2024-05-04 15:47:39 +0200

    create: alert side bar

---
+++ src/pages/editRecipe/components/helper/AlertSideBar.tsx
@@ -0,0 +1,42 @@
+import CloseIcon from '@mui/icons-material/Close';
+import { Alert, Box, IconButton } from '@mui/material';
+import { isMobile } from 'react-device-detect';
+
+interface AlertSideBarProps {
+    isOpenErrorMessage: boolean;
+    errorMessage: string;
+    setIsOpenErrorMessage: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function AlertSideBar({isOpenErrorMessage, errorMessage, setIsOpenErrorMessage}: AlertSideBarProps) {
+
+    return (<Box
+        sx={{
+            position: 'fixed',
+            bottom: isMobile ? 'unset' : '10px',
+            top: isMobile ? '10px' : 'unset',
+            left: '10px',
+            right: '10px',
+            width: isMobile ? 'calc(100% - 20px)' : '27%',
+            zIndex: 9999,
+        }}
+        className={isOpenErrorMessage ? 'error-message open' : 'error-message'}
+    >
+        <Alert
+            severity="error"
+            action={
+                <IconButton
+                    aria-label="close"
+                    color="inherit"
+                    size="small"
+                    onClick={()=> setIsOpenErrorMessage(false)}
+                >
+                    <CloseIcon fontSize="inherit" />
+                </IconButton>
+            }
+        >
+            {errorMessage}
+        </Alert>
+    </Box>
+    )
+}
